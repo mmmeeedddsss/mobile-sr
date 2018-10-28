2daafd8250e357296b3c6ba77cab80b0eeb34884
Mert Tunc <mert.tunc@udemy.com>
2018-10-28 22:01:37 +0300

    Added ability to select photo from gallery

--- android/app/src/main/java/com/example/yuksel/mobilesr/PickPhotoActivity.java
+++ android/app/src/main/java/com/example/yuksel/mobilesr/PickPhotoActivity.java
@@ -1,13 +1,37 @@
 package com.example.yuksel.mobilesr;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PickPhotoActivity extends Activity {
+    private static final int PICK_IMAGE = 100;
+    private ImageView imageView;
+
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         //Get the view From pick_photo_activity
         setContentView(R.layout.pick_photo_activity);
+
+        imageView = findViewById(R.id.pick_photo_image_view);
+        pickImage();
+    }
+
+    public void pickImage() {
+        Intent gallery_intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery_intent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 }
