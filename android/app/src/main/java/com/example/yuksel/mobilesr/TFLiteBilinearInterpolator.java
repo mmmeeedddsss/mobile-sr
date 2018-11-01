1aae5f8e2aef846f994ae56b201b4b7e80227ee6
denizmsayin <e209441@metu.edu.tr>
2018-11-02 01:11:51 +0300

    added and switched to 512x512 interp.

--- android/app/src/main/java/com/example/yuksel/mobilesr/TFLiteBilinearInterpolator.java
+++ android/app/src/main/java/com/example/yuksel/mobilesr/TFLiteBilinearInterpolator.java
@@ -14,18 +14,18 @@ import java.nio.ByteOrder;
 
 // Class representing the bilinear interpolator built for TFLite.
 public class TFLiteBilinearInterpolator implements BitmapProcessor {
-    private static final String MODEL_PATH = "bilinear.lite";
+    private static final String MODEL_PATH = "bilinear512.tflite";
 
     private static final int SIZEOF_FLOAT = 4;
 
     private static final int INPUT_TENSOR_BATCH = 1;
-    private int INPUT_TENSOR_WIDTH = 32;
-    private int INPUT_TENSOR_HEIGHT = 32;
+    private int INPUT_TENSOR_WIDTH = 512;
+    private int INPUT_TENSOR_HEIGHT = 512;
     private static final int INPUT_TENSOR_CHANNELS = 3;
 
     private static final int OUTPUT_TENSOR_BATCH = 1;
-    private int OUTPUT_TENSOR_WIDTH = 64;
-    private int OUTPUT_TENSOR_HEIGHT = 64;
+    private int OUTPUT_TENSOR_WIDTH = 1024;
+    private int OUTPUT_TENSOR_HEIGHT = 1024;
     private static final int OUTPUT_TENSOR_CHANNELS = 3;
 
     // the tflite interpreter
