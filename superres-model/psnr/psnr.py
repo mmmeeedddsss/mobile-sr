fd2bdd07972a0b3a7c54f4ea77f4cc73b7db1a43
YASIN FATIH ALPUL <e2098739@ceng.metu.edu.tr>
2018-11-19 15:46:04 +0300

    Update psnr.py Ability to calc. given paths of images

--- superres-model/psnr/psnr.py
+++ superres-model/psnr/psnr.py
@@ -12,3 +12,17 @@ def calc_psnr_numpy(org, imit):
   mse = np.sum(sqerr)/dim
   psnr = 10 * math.log10(MAXVAL**2 / mse)
   return psnr
+
+# Calculate PSNR of two images given their file paths
+# Depends on calc_psnr_numpy
+# @params (original_image_path, imitation_image_path)
+# @ret float
+def calc_psnr_file_path(org, imit):
+  from PIL import Image
+  img1  = Image.open(org)
+  img2  = Image.open(imit)
+
+  img1 = np.array(img1)
+  img2 = np.array(img2)
+
+  return calc_psnr_numpy(img1, img2)
