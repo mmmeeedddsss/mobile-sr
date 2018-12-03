15721fab321964d790182c2da6557e29120c3f8b
yalpul <yalpul@gmail.com>
2018-12-03 23:33:38 +0300

    applying super-resolution

--- superres-model/eval/eval.py
+++ superres-model/eval/eval.py
@@ -26,7 +26,29 @@ def create_low_res(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
     for i in range(*bsd100_range):
       subprocess.call(cmd_for_downscale(bsd100_path+'{}.png'.format(i)), shell=True)
 
+def cmd_for_SR(file_path):
+  cmd = 'python bilinear.py {}'.format(file_path)
+  return cmd
+
+def apply_SR(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
+  set5_path = path + '/Set5/'
+  set14_path = path + '/Set14/'
+  bsd100_path = path + '/BSDS100/'
+  set5_range = (1, 6)
+  set14_range = (1, 15)
+  bsd100_range = (1, 101)
+  if set5_flag:
+    for i in range(*set5_range):
+      subprocess.call(cmd_for_SR(set5_path+'{}_LR.png'.format(i)), shell=True)
+  if set14_flag:
+    for i in range(*set14_range):
+      subprocess.call(cmd_for_SR(set14_path+'{}_LR.png'.format(i)), shell=True)
+  if bsd100_flag:
+    for i in range(*bsd100_range):
+      subprocess.call(cmd_for_SR(bsd100_path+'{}_LR.png'.format(i)), shell=True)
+  
 if __name__ == '__main__':
   path = sys.argv[1]
   create_low_res(path)
+  apply_SR(path)
 
