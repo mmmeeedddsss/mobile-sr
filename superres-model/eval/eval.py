ab68a8e89f26877d53fa3301c46fc6e2bac4cd14
yalpul <yalpul@gmail.com>
2018-12-04 00:13:38 +0300

    code refactoring

--- superres-model/eval/eval.py
+++ superres-model/eval/eval.py
@@ -1,9 +1,19 @@
 #! /usr/bin/python
 
+import numpy as np
 import sys
 import subprocess
 import psnr
 datasets = ['Set5', 'Set14', 'BSDS100']
+set5_range = (1, 6)
+set14_range = (1, 15)
+bsd100_range = (1, 101)
+
+def dataset_paths(path):
+  set5_path = path + '/Set5/'
+  set14_path = path + '/Set14/'
+  bsd100_path = path + '/BSDS100/'
+  return set5_path, set14_path, bsd100_path
 
 def cmd_for_downscale(file_path):
   new_path = file_path[:-4] + '_LR.png'
@@ -11,12 +21,7 @@ def cmd_for_downscale(file_path):
   return cmd
 
 def create_low_res(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
-  set5_path = path + '/Set5/'
-  set14_path = path + '/Set14/'
-  bsd100_path = path + '/BSDS100/'
-  set5_range = (1, 6)
-  set14_range = (1, 15)
-  bsd100_range = (1, 101)
+  set5_path, set14_path, bsd100_path = dataset_paths(path)
   if set5_flag:
     for i in range(*set5_range):
       subprocess.call(cmd_for_downscale(set5_path+'{}.png'.format(i)), shell=True)
@@ -32,12 +37,7 @@ def cmd_for_SR(file_path):
   return cmd
 
 def apply_SR(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
-  set5_path = path + '/Set5/'
-  set14_path = path + '/Set14/'
-  bsd100_path = path + '/BSDS100/'
-  set5_range = (1, 6)
-  set14_range = (1, 15)
-  bsd100_range = (1, 101)
+  set5_path, set14_path, bsd100_path = dataset_paths(path)
   if set5_flag:
     for i in range(*set5_range):
       subprocess.call(cmd_for_SR(set5_path+'{}_LR.png'.format(i)), shell=True)
@@ -49,35 +49,31 @@ def apply_SR(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
       subprocess.call(cmd_for_SR(bsd100_path+'{}_LR.png'.format(i)), shell=True)
   
 def calc_psnr_values(path, set5_flag=True, set14_flag=True, bsd100_flag=True):
-  set5_path = path + '/Set5/'
-  set14_path = path + '/Set14/'
-  bsd100_path = path + '/BSDS100/'
-  set5_range = (1, 6)
-  set14_range = (1, 15)
-  bsd100_range = (1, 101)
+  set5_path, set14_path, bsd100_path = dataset_paths(path)
   result = []
   if set5_flag:
     set5_sum = []
     for i in range(*set5_range):
-      set5_sum.append(\ 
-        psnr.calc_psnr_file_path(set5_path+'{}.png'.format(i),\ 
-                                 set5_path+'{}_LR_interp'.format(i)))
+      print i
+      set5_sum.append(\
+        psnr.calc_psnr_file_path(set5_path+'{}.png'.format(i),\
+                                 set5_path+'{}_LR_interp.png'.format(i)))
     set5_mean = np.mean(set5_sum)
     result.append(set5_mean)
   if set14_flag:
     set14_sum = []
     for i in range(*set14_range):
-      set14_sum.append(\ 
-        psnr.calc_psnr_file_path(set14_path+'{}.png'.format(i),\ 
-                                 set14_path+'{}_LR_interp'.format(i)))
+      set14_sum.append(\
+        psnr.calc_psnr_file_path(set14_path+'{}.png'.format(i),\
+                                 set14_path+'{}_LR_interp.png'.format(i)))
     set14_mean = np.mean(set14_sum)
     result.append(set14_mean)
   if bsd100_flag:
     bsd100_sum = []
     for i in range(*bsd100_range):
-      bsd100_sum.append(\ 
-        psnr.calc_psnr_file_path(bsd100_path+'{}.png'.format(i),\ 
-                                 bsd100_path+'{}_LR_interp'.format(i)))
+      bsd100_sum.append(\
+        psnr.calc_psnr_file_path(bsd100_path+'{}.png'.format(i),\
+                                 bsd100_path+'{}_LR_interp.png'.format(i)))
     bsd100_mean = np.mean(bsd100_sum)
     result.append(bsd100_mean)
   return result
@@ -86,4 +82,5 @@ if __name__ == '__main__':
   path = sys.argv[1]
   create_low_res(path)
   apply_SR(path)
+  print(calc_psnr_values(path))
 
