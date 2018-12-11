cdadeff70f0454b46798e2258a16df1ef85f4942
denizmsayin <e209441@metu.edu.tr>
2018-12-11 23:48:06 +0300

    added reinitializable iterator for validation

--- superres-model/training/data.py
+++ superres-model/training/data.py
@@ -24,7 +24,7 @@ def normalize_pair(hr_img, lr_img):
     return hr_norm, lr_norm
 
 
-def create_png_pair_dataset(input_files, batch_size, num_epochs, opts):
+def create_png_pair_dataset(input_files, batch_size, opts):
     ''' a function to create a data loader from input files and parameters '''
     # create a tfrecorddataset from the input files
     dataset = tf.data.TFRecordDataset(input_files)
@@ -34,8 +34,6 @@ def create_png_pair_dataset(input_files, batch_size, num_epochs, opts):
     dataset = dataset.map(normalize_pair)
     # shuffle the dataset with a buffer size multiple of the batch size
     dataset = dataset.shuffle(opts['shuffle_multiplier'] * batch_size)
-    # repeat the dataset if necessary
-    dataset = dataset.repeat(num_epochs)
     # divide the dataset into batches
     dataset = dataset.batch(batch_size)
     # prefetch the dataset for quicker training execution
@@ -43,3 +41,30 @@ def create_png_pair_dataset(input_files, batch_size, num_epochs, opts):
     return dataset
 
 
+def create_combined_iterator(training_set, validation_set):
+    ''' a function that creates an iterator that can be reinitialized over both datasets '''
+    # create an iterator through defining the structure
+    itr = tf.data.Iterator.from_structure(training_set.output_types, 
+                                          training_set.output_shapes)
+    # create the tensor to get the next element (the actual iterator)
+    itr_next = itr.get_next()
+    # create a reinitialization op for the training & validation set
+    training_initializer = itr.make_initializer(training_set)
+    validation_initializer = itr.make_initializer(validation_set)
+    # return everything
+    return itr_next, training_initializer, validation_initializer
+
+
+def create_combined_data_loader(training_data_file, validation_data_file, 
+                                batch_size, opts):
+    ''' 
+    This function combines create_png_pair_dataset and create_combined_iterator to
+    create a data loader that can be reinitialized over both datasets, given the file
+    paths as input. The returned tuple contains an iterator tensor, as well as two
+    reinitialization ops. Batch_size and opts are used in the creation of both sets.
+    '''
+    # create both datasets and return their combined iterator
+    training_dataset = create_png_pair_dataset((training_data_file,), batch_size, opts)
+    validation_dataset = create_png_pair_dataset((validation_data_file,), batch_size, opts)
+    combined_loader = create_combined_iterator(training_dataset, validation_dataset)
+    return combined_loader
--- superres-model/training/trainer.py
+++ superres-model/training/trainer.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
+from itertools import count
+import os
 
 import tensorflow as tf
+import numpy as np
 
+from data import create_combined_data_loader
 from losses import mse_loss_layer
 from models import srcnn_x2_weak
 import train_opts as OPTS
@@ -10,9 +14,11 @@ import train_opts as OPTS
 def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument(
-        'input_files',
-        help='.tfrecord files storing png hr-lr pairs',
-        nargs='+')
+        'training_file',
+        help='.tfrecord file storing png hr-lr pairs for training')
+    parser.add_argument(
+        'validation_file',
+        help='.tfrecord file storing png hr-lr pairs for validation')
     parser.add_argument(
         '--learning-rate', '-l',
         help='learning rate for the optimizer. Defaults to 1e-3',
@@ -35,8 +41,10 @@ def parse_arguments():
 def wrap_model(data_loader, model, loss, optimizer, opts):
     # create global step
     global_step = tf.train.create_global_step()
+    # extract the data loader variables
+    iterator, training_initializer, validation_initializer = data_loader
     # load the data batches
-    input_hr_batch, input_lr_batch = data_loader()
+    input_hr_batch, input_lr_batch = iterator 
     # extra for naming
     input_lr_batch = tf.identity(input_lr_batch, name='input_image')
     # forward the lr batch through the model, also send the hr to infer ratio
@@ -45,7 +53,6 @@ def wrap_model(data_loader, model, loss, optimizer, opts):
     output_hr_batch = tf.identity(output_hr_batch, name='output_image')
     # get the losses through the input & output HR images
     losses = loss(input_hr_batch, output_hr_batch)
-    
     # add summaries for interesting variables
     tf.summary.scalar('loss', losses) # summary for the loss
     tf.summary.image('lr_patch', input_lr_batch) # summary for the input LR
@@ -62,15 +69,16 @@ def wrap_model(data_loader, model, loss, optimizer, opts):
     with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
         train_op = optimizer.minimize(losses, global_step=global_step)
     # return some useful variables
-    return input_lr_batch, output_hr_batch, losses, train_op, merged_summary
+    return input_lr_batch, output_hr_batch, losses, train_op, merged_summary, training_initializer, validation_initializer
 
 
-def train(model_vars, opts):
+def train(model_vars, num_epochs, opts):
     '''
     Train the given model on the given dataset.
     '''
     # unpack the model variables
-    input_lr_sym, output_hr_sym, loss_sym, train_op, merged_summary = model_vars
+    (input_lr_sym, output_hr_sym, loss_sym, train_op, 
+        merged_summary, train_init, val_init) = model_vars
     # create a saver & global step
     saver = tf.train.Saver()
     global_step = tf.train.get_global_step()
@@ -94,38 +102,73 @@ def train(model_vars, opts):
             # since we are not reloading, initialize global variables
             sess.run(tf.global_variables_initializer())
         try:
-            # create a summary file writer
-            with tf.summary.FileWriter(opts['log_dir'], sess.graph) as summ_writer:
-                # end of training is signaled by a tf.errors.OutOfRangeError
-                # exception, so our while loop runs forever
-                while True:
-                    # run an iteration, get the loss and step to print them out
-                    # also run on the train op to force gradient backprop
-                    _, loss, step = sess.run([train_op, loss_sym, global_step])
+            # create summary file writers
+            train_writer = tf.summary.FileWriter(os.path.join(opts['log_dir'], 'training'), 
+                                                             sess.graph)
+            val_writer = tf.summary.FileWriter(os.path.join(opts['log_dir'], 'validation'), 
+                                                            sess.graph)
+            with train_writer, val_writer:
+                # iterate over the training dataset num_epochs times
+                for _ in range(num_epochs):
+                    try:
+                        # run the training initializer
+                        sess.run(train_init)
+                        # end of training is signaled by a tf.errors.OutOfRangeError
+                        # exception, so our while loop runs forever
+                        while True:
+                            # run an iteration, get the loss and step to print them out
+                            # also run on the train op to force gradient backprop
+                            _, loss, step = sess.run([train_op, loss_sym, global_step])
 
-                    # print step & loss, if required by the step
-                    if step % opts['print_every'] == 0:
-                        print(f'Step {step}: loss {loss:.2e}')
+                            # print step & loss, if required by the step
+                            if step % opts['print_every'] == 0:
+                                print(f'Step {step}: loss {loss:.2e}')
 
-                    # save a checkpoint, if required by the step
-                    if step % opts['save_every'] == 0:
-                        saver.save(sess, opts['checkpoint_file'], global_step=global_step)
+                            # save a checkpoint, if required by the step
+                            if step % opts['save_every'] == 0:
+                                saver.save(sess, opts['checkpoint_file'], global_step=global_step)
 
-                    # log results, if required by the steps
-                    if step % opts['log_every'] == 0:
-                        # rerun to get a merged summary, without touching other ops
-                        summary = sess.run(merged_summary)
-                        # write the summary
-                        summ_writer.add_summary(summary, step)
-        # if the dataset is exhausted (tf.errors.OutOfRangeError) or
-        # the user presses Ctrl+C, fully save the model (different from checkpoints)
-        except (tf.errors.OutOfRangeError, KeyboardInterrupt):
-            print('Training done. Saving model...')
-            tf.saved_model.simple_save(
-                sess,
-                opts['model_dir'],
-                inputs={'input_image': input_lr_sym},
-                outputs={'output_image': output_hr_sym})
+                            # log results, if required by the steps
+                            if step % opts['log_every'] == 0:
+                                # rerun to get a merged summary, without touching other ops
+                                summary = sess.run(merged_summary)
+                                # write the summary
+                                train_writer.add_summary(summary, step)
+                    # wait for the dataset to be exhausted (tf.errors.OutOfRangeError)
+                    except tf.errors.OutOfRangeError:
+                        # now, record the validation error
+                        print('End of epoch reached. Calculating validation error...')
+                        try:
+                            # run the validation initializer
+                            sess.run(val_init)
+                            # run over the whole set again, calculating losses and counting
+                            losses = []
+                            vc = count(0)
+                            while True:
+                                print(f'\rProcessing validation batch {next(vc)}...', end='')
+                                # calculate the loss of a batch
+                                loss = sess.run(loss_sym)
+                                # append it to losses
+                                losses.append(loss)
+                        # validation dataset is exhausted
+                        except tf.errors.OutOfRangeError: 
+                            # calculate the loss with numpy, print it
+                            avg_loss = np.mean(np.array(losses))
+                            print(f'Average validation loss: {avg_loss:.2e}')
+                            # record it by manually creating a Summary protobuf
+                            val_loss_summary = tf.Summary(value=[
+                                tf.Summary.Value(tag='loss', simple_value=avg_loss)])
+                            val_writer.add_summary(val_loss_summary, step)
+        # fully save the model (different from checkpoints), either
+        # when the training ends, or user presses Ctrl+C
+        except KeyboardInterrupt:
+            pass
+        print('Training done. Saving model...')
+        tf.saved_model.simple_save(
+            sess,
+            opts['model_dir'],
+            inputs={'input_image': input_lr_sym},
+            outputs={'output_image': output_hr_sym})
 
 
 def main():
@@ -135,10 +178,10 @@ def main():
     # get the command line arguments
     args = parse_arguments()
     # create parts of the model
-    data_loader = create_data_loader(
-        args.input_files, 
+    data_loader = create_combined_data_loader(
+        args.training_file, 
+        args.validation_file,
         args.batch_size, 
-        args.num_epochs, 
         OPTS.DATA_LOADER)
     model = srcnn_x2_weak
     loss = mse_loss_layer
@@ -146,7 +189,8 @@ def main():
     # combine the parts to create the full model
     model_vars = wrap_model(data_loader, model, loss, optimizer, OPTS.MODEL)
     # do the training
-    train(model_vars, OPTS.TRAIN)
+    train(model_vars, args.num_epochs, OPTS.TRAIN)
+
 
 if __name__ == '__main__':
     main()
