8ac8d003290c4a7bf093db5a9412d488e733d83d
denizmsayin <e209441@metu.edu.tr>
2018-12-12 20:37:10 +0300

    added extra input location to model

--- superres-model/training/convert_model.sh
+++ superres-model/training/convert_model.sh
@@ -1,16 +1,19 @@
 output_tflite=$1
+input_tensor="resized_image"
+output_tensor="output_image"
+
 
 printf "*****\nFreezing Graph\n*****\n"
 freeze_graph --input_saved_model_dir saved-model \
-    --output_node_names output_image \
+    --output_node_names $output_tensor \
     --output_graph frozen.pb
 
 printf "*****\nOptimizing Frozen Graph\n*****\n"
 python -m tensorflow.python.tools.optimize_for_inference \
     --input frozen.pb \
     --output optim.pb \
-    --input_names input_image \
-    --output_names output_image \
+    --input_names $input_tensor \
+    --output_names $output_tensor \
     --frozen_graph
 
 printf "*****\nConverting Optimized Graph to TFLite\n*****\n"
@@ -19,6 +22,6 @@ toco --graph_def_file optim.pb \
     --output_format TFLITE \
     --inference-type FLOAT \
     --inference_input_type FLOAT \
-    --input_arrays input_image \
-     --input_shapes 1,256,256,3 \
-     --output_arrays output_image
+    --input_arrays $input_tensor \
+     --input_shapes 1,128,128,3 \
+     --output_arrays $output_tensor
--- superres-model/training/models.py
+++ superres-model/training/models.py
@@ -4,6 +4,10 @@ def srcnn_x2_weak(lr_batch):
     # apply nn resize (as it is supported in TFLite)
     hr_shape = 2 * tf.shape(lr_batch)[1:3]
     lr_resized = tf.image.resize_nearest_neighbor(lr_batch, hr_shape)
+    # save the name of the resized_image for model modification
+    lr_resized = tf.identity(lr_resized, name='resized_image')
+    # add a summary for resized
+    tf.summary.image('resized_image', lr_resized)
     # first conv. layer
     c1 = tf.layers.conv2d(lr_resized, 64, 9, padding='SAME')
     # second conv. layer
