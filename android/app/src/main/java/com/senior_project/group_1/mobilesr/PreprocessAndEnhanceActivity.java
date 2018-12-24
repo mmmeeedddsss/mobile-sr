4dc2d5970495ce8f87984f209f28f068f11b9c9b
Mert Tunc <mert.tunc@udemy.com>
2018-12-25 01:11:20 +0300

    Added functionality to the settings screen

--- android/app/src/main/java/com/senior_project/group_1/mobilesr/PreprocessAndEnhanceActivity.java
+++ android/app/src/main/java/com/senior_project/group_1/mobilesr/PreprocessAndEnhanceActivity.java
@@ -68,7 +68,7 @@ public class PreprocessAndEnhanceActivity extends AppCompatActivity {
             long startTime = System.nanoTime();
             Log.i("processImage", String.format("Bitmap size: %d %d", bitmap.getWidth(), bitmap.getHeight()));
 
-            SRModelConfiguration modelConfiguration = SRModelConfigurationManager.getConfiguration("SRCNN_NR_256");
+            SRModelConfiguration modelConfiguration = SRModelConfigurationManager.getCurrentConfiguration();
 
             ImageProcessingHelper.divideImage(imageView.getCurrentBitmap());
             ImageProcessingHelper.processImages(this, modelConfiguration);
