da60919d66dbb091579e9ac4e8e636c3d90fb7f4
yalpul <yalpul@gmail.com>
2019-04-09 21:51:37 +0300

    improvements on implementation

--- superres-model/eval/server.py
+++ superres-model/eval/server.py
@@ -4,23 +4,29 @@ import sys
 
 ip = '0.0.0.0'
 port = 61274
+addr = (ip, port)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind( (ip, port) )
+sock.bind(addr)
 sock.listen(1)
+
+print('Listening on: ' + str(addr))
 
 clientSock, clientAddr = sock.accept()
 imageSize = int(clientSock.recv(10))
 print('Reading ' + str(imageSize) + ' bytes of data')
-imageData = ""
-readData = clientSock.recv(4096)
-while True:
-  imageData += readData
-  if len(imageData) == imageSize:
-    break
+try:
+  imageData = ""
   readData = clientSock.recv(4096)
-print(str(len(imageData)) + ' bytes read')
-with open('tmp', 'wb') as f:
-  f.write(imageData)
-clientSock.close()
-sock.close()
+  while True:
+    imageData += readData
+    if len(imageData) == imageSize:
+      break
+    readData = clientSock.recv(4096)
+  print(str(len(imageData)) + ' bytes read')
+  with open('low_res_image.png', 'wb') as f:
+    f.write(imageData)
+  os.system('python superresolve.py ../saved-model low_res_image.png')
+except:
+  clientSock.close()
+  sock.close()
