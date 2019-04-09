a577c10628cd2476f2e1fc32a34883654cb626c2
yalpul <yalpul@gmail.com>
2019-04-09 23:34:46 +0300

    comments and time measuring

--- superres-model/eval/server.py
+++ superres-model/eval/server.py
@@ -1,12 +1,17 @@
 import socket
 import os
+import time
 import subprocess
 import sys
 from argparse import ArgumentParser
 
+# default values
+# can be changed from command line
+# arguments when running
 ip = '0.0.0.0'
 port = 61274
 
+# argument parser
 def parse_arguments():
   parser = ArgumentParser()
   parser.add_argument(
@@ -21,6 +26,7 @@ def parse_arguments():
   args = parser.parse_args()
   return args
 
+# SR processing
 def process():
   proc = subprocess.Popen(["python", 'superresolve.py',
     '../saved-model', 'low_res_image.png'],
@@ -42,31 +48,48 @@ if __name__ == '__main__':
 
   print('Listening on: ' + str(addr))
 
-  try:
-    clientSock, clientAddr = sock.accept()
-    imageSize = int(clientSock.recv(10))
-    print('Reading ' + str(imageSize) + ' bytes of data')
-    imageData = ""
+  clientSock, clientAddr = sock.accept()
+  imageSize = int(clientSock.recv(10))
+  print('Reading ' + str(imageSize) + ' bytes of data')
+
+  # Get data from client
+  imageData = ""
+  readData = clientSock.recv(4096)
+  while True:
+    imageData += readData
+    if len(imageData) == imageSize:
+      break
     readData = clientSock.recv(4096)
-    while True:
-      imageData += readData
-      if len(imageData) == imageSize:
-        break
-      readData = clientSock.recv(4096)
-    print(str(len(imageData)) + ' bytes read')
-    with open('low_res_image.png', 'wb') as f:
-      f.write(imageData)
-    err = process()
-    if err and args.verbose:
-      print(err)
-    with open('sr-images/low_res_image-sr.png', 'rb') as srData:
-      buff = srData.read()
-      srSize = '%10s' % str(len(buff))
-      clientSock.send(srSize)
-      clientSock.send(buff)
-    os.system('rm -rf sr-images')
-  finally:
-    print('Closing connection...')
-    clientSock.close()
-    sock.close()
-    sys.exit()
+  print(str(len(imageData)) + ' bytes read')
+
+  # write data to file
+  with open('low_res_image.png', 'wb') as f:
+    f.write(imageData)
+
+  # apply SR on file
+  t = time.time()
+  err = process()
+  t = time.time() - t
+  if err and args.verbose:
+    print(err)
+
+  # send file back
+  print('Sending SR image...')
+  with open('sr-images/low_res_image-sr.png', 'rb') as srData:
+    buff = srData.read()
+    buffSize = len(buff)
+    buffSizeStr = str(buffSize)
+    print('Sending ' + buffSizeStr + ' bytes of data')
+    srSize = '%10s' % buffSizeStr
+    clientSock.send(srSize)
+    clientSock.send(buff)
+
+  print('File sent')
+  print('Time taken: ' + str(t))
+
+  # cleanup
+  os.system('rm -rf sr-images')
+  print('Closing connection...')
+  clientSock.close()
+  sock.close()
+  sys.exit()
