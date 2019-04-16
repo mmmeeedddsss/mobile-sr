6cfd06e121e0a0f65551a22a4f3f406e2e584b69
yalpul <yalpul@gmail.com>
2019-04-16 18:57:29 +0300

    multiple image ability and separate function

--- superres-model/eval/server.py
+++ superres-model/eval/server.py
@@ -11,6 +11,8 @@ from argparse import ArgumentParser
 ip = '0.0.0.0'
 port = 61274
 
+image_file_name = 'low_res_img.png'
+
 # argument parser
 def parse_arguments():
   parser = ArgumentParser()
@@ -29,10 +31,60 @@ def parse_arguments():
 # SR processing
 def process():
   proc = subprocess.Popen(["python", 'superresolve.py',
-    '../saved-model', 'low_res_image.png'],
+    '../saved-model', image_file_name],
     stdout=subprocess.PIPE,stderr=subprocess.PIPE)
   (out, err) = proc.communicate()
   return err
+
+def handle_request(clientSock):
+  imageSize = int(clientSock.recv(10))
+  if imageSize == 0:
+    return True
+  print('Reading ' + str(imageSize) + ' bytes of data...')
+
+  # Get data from client
+  imageData = ""
+  sizetoread = imageSize
+  readData = clientSock.recv(sizetoread)
+  while True:
+    imageData += readData
+    readSize = len(imageData)
+    if readSize == imageSize:
+      break
+    sizetoread = imageSize-readSize
+    readData = clientSock.recv(sizetoread)
+  print(str(len(imageData)) + ' bytes read.')
+
+  # write data to file
+  with open(image_file_name, 'wb') as f:
+    f.write(imageData)
+
+  # apply SR on file
+  print('Superresolution started...')
+  t = time.time()
+  err = process()
+  t = time.time() - t
+  if err and args.verbose:
+    print(err)
+  print('Superresolution finished.')
+
+  # send file back
+  print('Sending SR image...')
+  with open('sr-images/'+image_file_name+'-sr.png', 'rb') as srData:
+    buff = srData.read()
+    buffSize = len(buff)
+    buffSizeStr = str(buffSize)
+    print('Sending ' + buffSizeStr + ' bytes of data')
+    srSize = '%10s' % buffSizeStr
+    clientSock.send(srSize)
+    clientSock.send(buff)
+
+  print('File sent.')
+  print('Time taken: ' + str(t))
+
+  # cleanup
+  os.system('rm -rf sr-images')
+  return False
 
 if __name__ == '__main__':
   args = parse_arguments()
@@ -49,46 +101,9 @@ if __name__ == '__main__':
   print('Listening on: ' + str(addr))
 
   clientSock, clientAddr = sock.accept()
-  imageSize = int(clientSock.recv(10))
-  print('Reading ' + str(imageSize) + ' bytes of data')
-
-  # Get data from client
-  imageData = ""
-  readData = clientSock.recv(4096)
-  while True:
-    imageData += readData
-    if len(imageData) == imageSize:
-      break
-    readData = clientSock.recv(4096)
-  print(str(len(imageData)) + ' bytes read')
-
-  # write data to file
-  with open('low_res_image.png', 'wb') as f:
-    f.write(imageData)
-
-  # apply SR on file
-  t = time.time()
-  err = process()
-  t = time.time() - t
-  if err and args.verbose:
-    print(err)
-
-  # send file back
-  print('Sending SR image...')
-  with open('sr-images/low_res_image-sr.png', 'rb') as srData:
-    buff = srData.read()
-    buffSize = len(buff)
-    buffSizeStr = str(buffSize)
-    print('Sending ' + buffSizeStr + ' bytes of data')
-    srSize = '%10s' % buffSizeStr
-    clientSock.send(srSize)
-    clientSock.send(buff)
-
-  print('File sent')
-  print('Time taken: ' + str(t))
-
-  # cleanup
-  os.system('rm -rf sr-images')
+  completed = False
+  while not completed:
+    completed = handle_request(clientSock)
   print('Closing connection...')
   clientSock.close()
   sock.close()
