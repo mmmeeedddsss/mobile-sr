d1e4ddd9cc7e4d757e109846e80c7ea90d95fb59
Deniz MERCADIER SAYIN <e2094415@ceng.metu.edu.tr>
2019-04-30 23:08:58 +0300

    tried two inputs

--- superres-model/vgg-test/mytest.py
+++ superres-model/vgg-test/mytest.py
@@ -22,16 +22,28 @@ i2 = np.array(i2).astype('float32') / 255.0
 
 # build the network
 inp = tf.placeholder(tf.float32, [1, 224, 224, 3])
-vgg = Vgg19()
-vgg.build(inp)
+inp2 = tf.placeholder(tf.float32, [1, 224, 224, 3])
+
+with tf.variable_scope('vgg19'):
+    vgg = Vgg19()
+    vgg.build(inp)
+with tf.variable_scope('vgg19', reuse=True):
+    vgg2 = Vgg19()
+    vgg2.build(inp2)
+
+tf.summary.image('i1', inp)
+tf.summary.image('i2', inp2)
+summary = tf.summary.merge_all()
 
 # run the images through the network
 with tf.Session() as sess:
-    o1 = sess.run(vgg.conv5_4, feed_dict={inp: i1.reshape(1, 224, 224, 3)})
-    o2 = sess.run(vgg.conv5_4, feed_dict={inp: i2.reshape(1, 224, 224, 3)})
+    with tf.summary.FileWriter('log/test', sess.graph) as writer:
+        summ = sess.run(summary, feed_dict={inp: i1.reshape(1, 224, 224, 3), 
+                                            inp2: i2.reshape(1, 224, 224, 3)})
+        writer.add_summary(summ, 1)
 
-print(o1.shape)
-print(o2.shape)
-
-print(np.sum(o1 - o2))
-
+    tf.saved_model.simple_save(
+        sess,
+        'model',
+        inputs={'i1': inp, 'i2': inp2},
+        outputs={'o1': vgg.conv1_1, 'o2': vgg.conv1_1})
